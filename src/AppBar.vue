e32e75d93466a72a70adb7aaf0066b59fa9ba7d0
Jens Grønhøj Stigaard <jens@stigaard.info>
2020-11-26 20:11:20 +0100

    Made app bar more dense

--- src/AppBar.vue
+++ src/AppBar.vue
@@ -1,19 +1,20 @@
 <template lang="pug">
-v-app-bar(app color="primary" dark)
-	div: b Simple vMix switcher
-	v-spacer
-	v-combobox(
-		v-model="host"
-		label="vMix host"
-		:items="previousHosts"
-		@keyup.enter="changeHost"
-		@blur="changeHost"
-		:hide-selected="true"
-	).mt-8
-	v-icon#connection-status.ml-2.mt-2(
-		small 
-		:class="$vMixConnection.connected?'green--text':'red--text'" 
-	) fa-circle
+v-app-bar(app, dark, dense, color='primary')
+  div: b Simple vMix switcher
+  v-spacer
+  v-combobox.mt-7(
+    v-model='host',
+    label='vMix host',
+    dense,
+    :items='previousHosts',
+    @keyup.enter='changeHost',
+    @blur='changeHost',
+    :hide-selected='true'
+  )
+  v-icon#connection-status.ml-2.mt-2(
+    small,
+    :class='$vMixConnection.connected ? "green--text" : "red--text"'
+  ) fa-circle
 </template>
 
 <script lang="ts">
@@ -22,7 +23,7 @@ import Component from 'vue-class-component'
 import { Watch, Prop } from 'vue-property-decorator'
 
 @Component({
-  components: {}
+  components: {},
 })
 export default class AppBar extends Vue {
   host: string = ''
